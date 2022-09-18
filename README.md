24658061c3f4d7f77cdee545262a91500b475f72
Tejs1 <67797070+Tejs1@users.noreply.github.com>
2022-09-19 00:56:12 +0530

    Module pattern

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
-# node-degzme
+# Module Pattern
+
+## Add 'type' : 'module' in package.json
+
+## Then you can export frunction from different file
+
+### How to run
+
+### run node index.js else
 
 [Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-degzme)
--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-// run `node index.js` in the terminal
+import { sum, subtract, divide, multiply } from './math.js';
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+console.log('Sum', sum(1, 2));
+console.log('Subtract', subtract(1, 2));
+console.log('Divide', divide(1, 2));
+console.log('Multiply', multiply(1, 2));
